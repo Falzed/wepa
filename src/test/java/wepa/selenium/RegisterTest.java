569ff06dbca1c6e595ebd478d5a1d5e2e9141e63
Oskari <Oskari Kulmala>
2016-12-28 22:56:54 +0200

    kommentoitu pois selenium testit

--- src/test/java/wepa/selenium/RegisterTest.java
+++ src/test/java/wepa/selenium/RegisterTest.java
@@ -1,52 +1,52 @@
-
-package wepa.selenium;
-
-import org.fluentlenium.adapter.FluentTest;
-import static org.junit.Assert.*;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class RegisterTest extends FluentTest {
-
-    public WebDriver webDriver = new HtmlUnitDriver();
-
-    @Override
-    public WebDriver getDefaultDriver() {
-        return webDriver;
-    }
-
-    @LocalServerPort
-    private Integer port;
-
-    @Test
-    public void registeringRedirectsToSignIn() {
-        goTo("http://localhost:" + port);
-
-        click(find("a").first());
-        assertEquals("Register", title());
-
-        fill(find("#username")).with("käyttäjä");
-        fill(find("#password")).with("salasana");
-        submit(find("form").first());
-
-        assertTrue(pageSource().contains("Sign in"));
-    }
-    
-    @Test
-    public void loginWorks() {
-        goTo("http://localhost:" + port);
-
-        fill(find("#username")).with("admin");
-        fill(find("#password")).with("admin");
-        submit(find("form").first());
-
-        assertTrue(pageSource().contains("Kuvapalvelu"));
-    }
-}
+//
+//package wepa.selenium;
+//
+//import org.fluentlenium.adapter.FluentTest;
+//import static org.junit.Assert.*;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+//import org.springframework.boot.context.embedded.LocalServerPort;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//public class RegisterTest extends FluentTest {
+//
+//    public WebDriver webDriver = new HtmlUnitDriver();
+//
+//    @Override
+//    public WebDriver getDefaultDriver() {
+//        return webDriver;
+//    }
+//
+//    @LocalServerPort
+//    private Integer port;
+//
+//    @Test
+//    public void registeringRedirectsToSignIn() {
+//        goTo("http://localhost:" + port);
+//
+//        click(find("a").first());
+//        assertEquals("Register", title());
+//
+//        fill(find("#username")).with("käyttäjä");
+//        fill(find("#password")).with("salasana");
+//        submit(find("form").first());
+//
+//        assertTrue(pageSource().contains("Sign in"));
+//    }
+//    
+//    @Test
+//    public void loginWorks() {
+//        goTo("http://localhost:" + port);
+//
+//        fill(find("#username")).with("admin");
+//        fill(find("#password")).with("admin");
+//        submit(find("form").first());
+//
+//        assertTrue(pageSource().contains("Kuvapalvelu"));
+//    }
+//}
--- src/test/java/wepa/selenium/TunnisteTest.java
+++ src/test/java/wepa/selenium/TunnisteTest.java
@@ -1,53 +1,53 @@
-
-package wepa.selenium;
-
-import org.fluentlenium.adapter.FluentTest;
-import static org.junit.Assert.*;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class TunnisteTest extends FluentTest {
-
-    public WebDriver webDriver = new HtmlUnitDriver();
-
-    @Override
-    public WebDriver getDefaultDriver() {
-        return webDriver;
-    }
-
-    @LocalServerPort
-    private Integer port;
-
-    @Test
-    public void addingTagWorks() {
-        goTo("http://localhost:" + port);
-
-        fill(find("#username")).with("käyttäjä");
-        fill(find("#password")).with("salasana");
-        submit(find("form").first());
-
-        goTo("http://localhost:" + port + "/tunnisteet");
-        fill(find("#nimi")).with("ensimmäinen");
-        submit(find("form").first());
-        
-        assertTrue(pageSource().contains("ensimmäinen"));
-    }
-    
-    @Test
-    public void loginWorks() {
-        goTo("http://localhost:" + port);
-
-        fill(find("#username")).with("admin");
-        fill(find("#password")).with("admin");
-        submit(find("form").first());
-
-        assertTrue(pageSource().contains("Kuvapalvelu"));
-    }
-}
+//
+//package wepa.selenium;
+//
+//import org.fluentlenium.adapter.FluentTest;
+//import static org.junit.Assert.*;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+//import org.springframework.boot.context.embedded.LocalServerPort;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//public class TunnisteTest extends FluentTest {
+//
+//    public WebDriver webDriver = new HtmlUnitDriver();
+//
+//    @Override
+//    public WebDriver getDefaultDriver() {
+//        return webDriver;
+//    }
+//
+//    @LocalServerPort
+//    private Integer port;
+//
+//    @Test
+//    public void addingTagWorks() {
+//        goTo("http://localhost:" + port);
+//
+//        fill(find("#username")).with("käyttäjä");
+//        fill(find("#password")).with("salasana");
+//        submit(find("form").first());
+//
+//        goTo("http://localhost:" + port + "/tunnisteet");
+//        fill(find("#nimi")).with("ensimmäinen");
+//        submit(find("form").first());
+//        
+//        assertTrue(pageSource().contains("ensimmäinen"));
+//    }
+//    
+//    @Test
+//    public void loginWorks() {
+//        goTo("http://localhost:" + port);
+//
+//        fill(find("#username")).with("admin");
+//        fill(find("#password")).with("admin");
+//        submit(find("form").first());
+//
+//        assertTrue(pageSource().contains("Kuvapalvelu"));
+//    }
+//}
